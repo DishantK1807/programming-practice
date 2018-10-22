b48da4ac64338403874785c9046f46cbece69b2d
dishantk1807 <dishant.khanna1807@gmail.com>
2018-10-22 15:56:39 +0530

    Update: Update documentation for BubbleSort C-Program

--- Algorithms/Bubble Sort/C Program/bubble_sort.c
+++ Algorithms/Bubble Sort/C Program/bubble_sort.c
@@ -1,15 +1,23 @@
+/*
+Bubble Sort Program in C Programming Language
+
+The code is based on CS50 library, from Harvard CS50 Course, Introduction to Computer Science
+
+By: Dishant Khanna [dishant.khanna1807@gmail.com, dishant@dishantkhanna.me]
+*/
+
 #include<cs50.h>
 #include<stdio.h>
 
-void bubble_sort(int *arr, int arr_size);
-void swap(int *element, int *array_item);
-void print_array(int *arr, int arr_size);
-void take_array(int *arr, int arr_size);
+void bubble_sort(int *arr, int arr_size);   //declaration for bubble sort function
+void swap(int *element, int *array_item);   //declaration for swap function
+void print_array(int *arr, int arr_size);   //declaration for Print Array function
+void take_array(int *arr, int arr_size);    //declaration for Get array input function
 
 int main(void)
 {
     int a[10];
-    int size = get_int("Enter Size of the Array: ");
+    int size = get_int("Enter Size of the Array: ");    // Get Array Size
     take_array(a, size);
     printf("sorted array: \n");
     bubble_sort(a, size);
@@ -18,6 +26,10 @@ int main(void)
 
 }
 
+/*
+Function defination for swapping array elements
+Args: Element to swap, Array Element
+*/
 void swap(int *element, int *array_item)
 {
     int temp;
@@ -26,18 +38,31 @@ void swap(int *element, int *array_item)
     *array_item = temp;
 }
 
+/*
+Function to Print Array elements
+Args: Array, Size of array
+*/
 void print_array(int *arr, int arr_size)
 {
    for (int i = 0; i < arr_size; i++)
        printf("%i ", arr[i]);
 }
 
+/*
+Function to print array
+Args: Array, Size of array
+*/
 void take_array(int *arr, int arr_size)
 {
     for (int i = 0; i < arr_size; i++)
        scanf("%i ", &arr[i]);
 }
 
+/*
+Function To perform Bubble Sort
+
+Args: Array, Size of Array
+*/
 void bubble_sort(int *arr, int arr_size)
 {
     int temp;
