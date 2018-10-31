82bd31dcc7ba002aadb755d487792339ef659782
dishantk1807 <dishant.khanna1807@gmail.com>
2018-10-31 12:27:15 +0530

    Update: Documentation of C Program for insertion sort

--- Algorithms/Insertions Sort/C Program/insertion_sort.c
+++ Algorithms/Insertions Sort/C Program/insertion_sort.c
@@ -1,10 +1,18 @@
+/*
+Insertion Sort Program in C Programming Language
+
+The code is based on CS50 library, from Harvard CS50 Course, Introduction to Computer Science
+
+By: Dishant Khanna [dishant.khanna1807@gmail.com, dishant@dishantkhanna.me]
+*/
+
 #include<cs50.h>
 #include<stdio.h>
 
-void insertion_sort(int *arr, int arr_size);
-void swap(int *element, int *array_item);
-void print_array(int *arr, int arr_size);
-void take_array(int *arr, int arr_size);
+void insertion_sort(int *arr, int arr_size);  //declaration of bubble sort function
+void swap(int *element, int *array_item);  // declaration of swap function
+void print_array(int *arr, int arr_size);  // declaration of print array function
+void take_array(int *arr, int arr_size);  // declaration of take_array function to get array as input
 
 int main(void)
 {
@@ -18,6 +26,10 @@ int main(void)
 
 }
 
+/*
+Function defination for swapping array elements
+Args: Element to swap, Array Element
+*/
 void swap(int *element, int *array_item)
 {
     int temp;
@@ -26,18 +38,31 @@ void swap(int *element, int *array_item)
     *array_item = temp;
 }
 
+/*
+Function to Print Array elements
+Args: Array, Size of array
+*/
 void print_array(int *arr, int arr_size)
 {
    for (int i = 0; i < arr_size; i++)
        printf("%i ", arr[i]);
 }
 
+/*
+Function to print array
+Args: Array, Size of array
+*/
 void take_array(int *arr, int arr_size)
 {
     for (int i = 0; i < arr_size; i++)
        arr[i] = get_int();
 }
 
+/*
+Function To perform Selection Sort
+
+Args: Array, Size of Array
+*/
 void insertion_sort(int *arr, int arr_size)
 {
     int j =0,key = 0;
