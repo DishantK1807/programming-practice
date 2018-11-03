53733ff349477a25d0f17f1b5b4c16a9ce71aa7a
dishantk1807 <dishant.khanna1807@gmail.com>
2018-11-03 11:15:37 +0530

    Add: Documentation for Binary search C-Program

---
+++ Algorithms/Binary Search/C Program/README.md
@@ -0,0 +1,2 @@
+This Folder contains C Program for Binary Search algorithm.
+The code is based on CS50 library, from Harvard CS50 Course, Introduction to Computer Science
--- Algorithms/Binary Search/C Program/binary_search.c
+++ Algorithms/Binary Search/C Program/binary_search.c
@@ -1,10 +1,18 @@
+/*
+Linear Search Program in C Programming Language
+
+The code is based on CS50 library, from Harvard CS50 Course, Introduction to Computer Science
+
+By: Dishant Khanna [dishant.khanna1807@gmail.com, dishant@dishantkhanna.me]
+*/
+
 #include<cs50.h>
 #include<stdio.h>
 
-void take_array(int *arr, int arr_size);
-void bubble_sort(int *arr, int arr_size);
-void swap(int *element, int *array_item);
-int binary_search(int *arr, int arr_size, int element);
+void take_array(int *arr, int arr_size);  //declaration for Get array input function
+void bubble_sort(int *arr, int arr_size);  //declaration for bubble sort function
+void swap(int *element, int *array_item);  //declaration for swap function
+int binary_search(int *arr, int arr_size, int element);  //declaration for binary search function
 
 
 int main(void)
@@ -26,14 +34,21 @@ int main(void)
 
 }
 
-
+/*
+Function to get array input
+Args: Array, Size of array
+*/
 void take_array (int *arr, int arr_size)
 {
     for (int i = 0; i < arr_size; i++)
        arr[i] = get_int();
 }
 
+/*
+Function To perform Bubble Sort
 
+Args: Array, Size of Array
+*/
 void bubble_sort(int *arr, int arr_size)
 {
     int temp;
@@ -47,7 +62,10 @@ void bubble_sort(int *arr, int arr_size)
     }
 }
 
-
+/*
+Function defination for swapping array elements
+Args: Element to swap, Array Element
+*/
 void swap(int *element, int *array_item)
 {
     int temp;
@@ -56,7 +74,10 @@ void swap(int *element, int *array_item)
     *array_item = temp;
 }
 
-
+/*
+Function to implement binary search
+Args: Array, Size of array, Element
+*/
 int binary_search (int *arr, int arr_size, int element)
 {
     bubble_sort(arr, arr_size);
